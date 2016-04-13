5af95889ac6500bb7297117c36fc84f989594dd0
le roncier <leroncier@gmail.com>
2016-04-13 21:11:28 +0200

    Last commit of the day

--- app/index.js
+++ app/index.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var app = express();
 
+var exphbs  = require('express-handlebars');
+
+// setting up a view engine and configure it, like a very small package
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main',
+	layOutsDir: "./app/views/layouts"
+	 }));
+
+app.set('views', './app/views');
+app.set('view engine', 'handlebars');
+
+// call the motivations
+const motivations = require('motivations');
+
+// choose one 
+const pickOne = require('pick-one');
+
 app.get("/", function (request, response) {
-	response.send("Hey I like your shoes!");
+	response.render('motivation');
 });
 
 app.get("/about", function (request, response) {
-	response.send("C'est ma page");
+	response.send("Poop");
 });
 
 module.exports = app;
